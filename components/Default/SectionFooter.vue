6339d90dab44039c4396d8f85b5c5caeff323843
Gerry Gao <gao88915@gmail.com>
2021-04-20 11:32:13 +1000

    init

---
+++ components/Default/SectionFooter.vue
@@ -0,0 +1,30 @@
+<template>
+  <section id="footer">
+    <p>© - {{ fullYear }} Gerry Gao</p>
+  </section>
+</template>
+
+<script>
+export default {
+  computed: {
+    fullYear() {
+      return new Date().getFullYear()
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+#footer {
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  bottom: 0;
+  left: 0;
+  width: 100vw;
+  padding: 1rem 1rem;
+  background-color: #000;
+  color: white;
+}
+</style>
---
+++ components/Default/SectionHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <section id="header">
+    <button>Button</button>
+    <h1>JamStart</h1>
+    <div class="logo"></div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+#header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 80px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: #000;
+  color: white;
+}
+.logo {
+  background: url(/github.svg);
+  background-position: center center;
+  background-repeat: no-repeat;
+  width: 32px;
+  height: 32px;
+}
+button {
+  background-color: greenyellow;
+  color: black;
+  border: none;
+  padding: 0.5rem 0.75rem;
+  border-radius: 5px;
+}
+</style>
